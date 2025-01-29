deaa9d7c3b7c1aad65dfc01b74973d22caba9522
saratrasv <cyrondelahoz@gmail.com>
2025-01-29 07:40:13 -0400

    feat: add principal sets like tabs and timeout command

--- lua/hozyes/init.lua
+++ lua/hozyes/init.lua
@@ -1,2 +1,4 @@
 require("hozyes.remap")
 require("hozyes.colors")
+require("hozyes.lazy_init")
+require("hozyes.set")
---
+++ lua/hozyes/set.lua
@@ -0,0 +1,38 @@
+--theprimeagen
+
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 12
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50 
+
+vim.opt.colorcolumn = "80"
+
+--My own
+vim.o.timeout = true       -- Habilita el tiempo de espera
+vim.o.timeoutlen = 300 -- Reduce el tiempo de espera a 200 ms (ajústalo según tu preferencia)
+vim.o.ttimeoutlen = 0     -- Tiempo de espera para secuencias de escape
