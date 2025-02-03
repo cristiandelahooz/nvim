49e1bb83d542d132855dc4243da5f2a8fe6eaf58
cristiandelahoz <cyrondelahoz@gmail.com>
2025-02-03 06:52:53 -0400

    refactor(telescope): change initial mode to normal

--- lua/hozyes/lazy/telescope.lua
+++ lua/hozyes/lazy/telescope.lua
@@ -8,7 +8,11 @@ return {
     },
 
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup({
+            defaults = {
+                initial_mode = "normal"
+            }
+        })
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
@@ -27,4 +31,3 @@ return {
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 }
-
