9fc39a19bf9bfff41d2fbc53014d4027f4762f03
cristiandelahoz <cyrondelahoz@gmail.com>
2025-02-05 22:14:53 -0400

    refactor(remap): change keymap to exit insert mode to 'qj'

--- lua/hozyes/remap.lua
+++ lua/hozyes/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("i", "jj", "<Esc>:w<CR>")
+vim.keymap.set("i", "qj", "<Esc>:w<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
