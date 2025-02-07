2633b3a16821e3205af5c5fcb28508de1889f381
cristiandelahoz <cyrondelahoz@gmail.com>
2025-02-07 08:43:20 -0400

    feat(nvim-tree): add personal preferences

--- lua/hozyes/lazy/nvim-tree.lua
+++ lua/hozyes/lazy/nvim-tree.lua
@@ -14,6 +14,40 @@ return {
         },
       }
     },
+    on_attach = function(bufnr)
+      local api = require "nvim-tree.api"
+
+      local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      -- Default mappings
+      api.config.mappings.default_on_attach(bufnr)
+
+      -- Custom mappings
+      vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
+      vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+
+      -- Open folder on moving right
+      vim.keymap.set('n', 'l', function()
+        local node = api.tree.get_node_under_cursor()
+        if node and node.type == "directory" and not node.open then
+          api.node.open.edit()
+        else
+          vim.cmd("normal! l")
+        end
+      end, opts('Open Folder or Move Right'))
+
+      -- Close folder on moving left
+      vim.keymap.set('n', 'h', function()
+        local node = api.tree.get_node_under_cursor()
+        if node and node.type == "directory" and node.open then
+          api.node.open.edit()
+        else
+          vim.cmd("normal! h")
+        end
+      end, opts('Close Folder or Move Left'))
+    end,
   },
   config = function (_, opts)
     -- Recommended settings to disable default netrw file explorer
