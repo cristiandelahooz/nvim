616be38c6fa069f59afc6eec294b7094c72e7d37
cristiandelahoz <cyrondelahoz@gmail.com>
2025-02-11 09:07:48 -0400

    feat(plugins): add snacks

---
+++ lua/hozyes/lazy/snacks.lua
@@ -0,0 +1,42 @@
+return {
+  "folke/snacks.nvim",
+  dependencies = {
+    "echasnovski/mini.icons",
+  },
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    dashboard = {
+      enabled = true,
+      preset = {
+        header = [[
+                                                                     
+       ████ ██████           █████      ██                     
+      ███████████             █████                             
+      █████████ ███████████████████ ███   ███████████   
+     █████████  ███    █████████████ █████ ██████████████   
+    █████████ ██████████ █████████ █████ █████ ████ █████   
+  ███████████ ███    ███ █████████ █████ █████ ████ █████  
+ ██████  █████████████████████ ████ █████ █████ ████ ██████ 
+        ]],
+      },
+    },
+    indent = { enabled = true },
+    input = { enabled = true },
+    git = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+  keys = {
+    { "<leader>sf",       function() Snacks.scratch() end,            desc = "Toggle Scratch Buffer" },
+    { "<leader>S",        function() Snacks.scratch.select() end,     desc = "Select Scratch Buffer" },
+    { "<leader><leader>", function() Snacks.picker.recent() end,      desc = "Recent Files" },
+    { "<leader>fb",       function() Snacks.picker.buffers() end,     desc = "Buffers" },
+    { "<leader>fg",       function() Snacks.picker.grep() end,        desc = "Grep Files" },
+  }
+}
