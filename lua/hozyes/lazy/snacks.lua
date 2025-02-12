4953c8e374c3dd34ff44bb9116acf1d8a0c9a071
cristiandelahoz <cyrondelahoz@gmail.com>
2025-02-12 07:24:33 -0400

    refactor(snacks): remove notifications

--- lua/hozyes/lazy/snacks.lua
+++ lua/hozyes/lazy/snacks.lua
@@ -25,7 +25,6 @@ return {
     input = { enabled = true },
     git = { enabled = true },
     picker = { enabled = true },
-    notifier = { enabled = true },
     quickfile = { enabled = true },
     scroll = { enabled = false },
     statuscolumn = { enabled = true },
