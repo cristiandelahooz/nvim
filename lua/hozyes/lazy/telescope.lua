31b310c8f63ff0a846a5e9962ba536710f9ccc33
cristiandelahoz <cyrondelahoz@gmail.com>
2025-02-27 00:31:40 -0400

    feat(telescope): add file ignore patterns to exclude unnecessary files

--- lua/hozyes/lazy/telescope.lua
+++ lua/hozyes/lazy/telescope.lua
@@ -10,8 +10,21 @@ return {
     config = function()
         require('telescope').setup({
             defaults = {
-                initial_mode = "normal"
-            }
+                initial_mode = "normal",
+               file_ignore_patterns = {
+                     ".git/",
+                     "node_modules/",
+                     "vendor/",
+                     "target/",
+                     "dist/",
+                     "build/",
+                     "deps/",
+                     "out/",
+                     "tmp/",
+                     ".*%.swp$",
+                    ".*%.class$",
+            },
+            },
         })
 
         local builtin = require('telescope.builtin')
