298d5a9e8f29374154a17dcf4bdb3127b11bcc9d
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-01 13:23:02 -0400

    refactor(none-ls): remove biome and add google format just for java

--- lua/hozyes/lazy/none-ls.lua
+++ lua/hozyes/lazy/none-ls.lua
@@ -3,14 +3,18 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local null_ls = require("null-ls")
+    local nls = null_ls.builtins
 
+    -- Configuración de fuentes
+    local sources = {
+      nls.formatting.google_java_format.with({
+        filetypes = { "java" },
+      }),
+    }
+
+    -- Configuración de null-ls
     null_ls.setup({
-      sources = {
-        -- Biome como formateador
-        null_ls.builtins.formatting.biome,
-        --java formatter
-        null_ls.builtins.formatting.google_java_format,
-      },
+      sources = sources,
     })
   end,
 }
