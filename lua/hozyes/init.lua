eb854ee4b465b21ef0bcf38c7af7bb40b71ad87e
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-01 21:57:18 -0400

    feat(core): add init commands

--- lua/hozyes/init.lua
+++ lua/hozyes/init.lua
@@ -1,3 +1,75 @@
 require("hozyes.remap")
 require("hozyes.lazy_init")
 require("hozyes.set")
+
+
+local augroup = vim.api.nvim_create_augroup
+local delahozGroup = augroup('delahoz', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+vim.filetype.add({
+    extension = {
+        templ = 'templ',
+    }
+})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+autocmd('BufRead', {
+    group = delahozGroup,
+    pattern = '*.conf',
+    command = "set filetype=apache",
+})
+autocmd({ "BufWritePre" }, {
+    group = delahozGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+autocmd('BufEnter', {
+    group = delahozGroup,
+    callback = function()
+        if vim.bo.filetype == "zig" then
+            vim.cmd.colorscheme("tokyonight-night")
+        else
+            vim.cmd.colorscheme("rose-pine")
+        end
+    end
+})
+
+
+autocmd('LspAttach', {
+    group = delahozGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "H", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+        vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>of", function() vim.diagostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>rf", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<M-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
+    end
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
