cb28ba5f3f77c35db0d7eec6d85b81594a7a8be0
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-02 14:30:22 -0400

    refactor(snacks): remove unused commands

--- lua/hozyes/lazy/snacks.lua
+++ lua/hozyes/lazy/snacks.lua
@@ -22,19 +22,10 @@ return {
         ]],
       },
     },
-    input = { enabled = true },
-    git = { enabled = true },
-    picker = { enabled = true },
-    quickfile = { enabled = true },
-    scroll = { enabled = false },
-    statuscolumn = { enabled = true },
-    words = { enabled = true },
   },
   keys = {
-        { "<leader>sf",       function() Snacks.scratch() end,       desc = "Toggle Scratch Buffer" },
         { "<leader><leader>", function() Snacks.picker.recent() end, desc = "Recent Files" },
-        { "<leader>ps",       function() Snacks.picker.grep() end,   desc = "Grep Files" },
-        { "<leader>pv",       function() Snacks.explorer() end,      desc = "Explorer" },
+
     }
 
 }
